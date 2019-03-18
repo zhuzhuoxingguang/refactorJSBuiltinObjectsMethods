f7110262c5be7c25b2e2e4cc90d6753e7b311afd
cauc <cauc@caucdeMacBook-Air.local>
2019-03-18 10:04:11 +0800

    file import and export

--- src/array/index.ts
+++ src/array/index.ts
@@ -5,5 +5,7 @@ let items = [
   { name: "jiang", age: 21 },
   { name: "web", age: 12 }
 ]
-let x = Utils.concat(items, [1, 2], [4, 3])
-console.log(items, x)
+let resultConcat = Utils.concat(items, [1, 2], [4, 3])
+console.log(items, resultConcat)
+let resultPush = Utils.push(items, 234)
+console.log(items, resultPush);
--- src/array/ts/index.ts
+++ src/array/ts/index.ts
@@ -4,12 +4,15 @@
  * 所以所有方法的第一个参数都是需要操作的数组
  */
 import concat from './concat'
+import push from './push'
 
 
 export {
-  concat
+  concat,
+  push
 }
 
 export default {
-  concat
+  concat,
+  push
 }
