fe3fcf69c4703a7167dff3a3ea107b3ed8cf4c9c
cauc <cauc@caucdeMacBook-Air.local>
2019-03-18 10:02:03 +0800

    array method push

---
+++ src/array/ts/push.ts
@@ -0,0 +1,15 @@
+/**
+ * push() 方法将一个或多个元素添加到数组的末尾，并返回该数组的新长度。
+ */
+/**
+ * @return 返回添加过新的元素的arr数组
+ * @param arr 需要操作的数组
+ * @param rest 需要添加到arr中的元素
+ */
+const push = (arr: any[], ...rest): number => {
+  for (let i = 0;i < rest.length;i++) {
+    arr[arr.length] = rest[i]
+  }
+  return arr.length
+}
+export default push
