9e5c20abf903f2859ac95d95300aebe976422051
zhuhuoxingguang <cauc@caucdeMacBook-Air.local>
2019-03-19 14:42:59 +0800

    js方式实现

---
+++ src/array/js/index.js
@@ -0,0 +1,274 @@
+/**
+ * 将所有方法封装到Utils对象内
+ * 所有方法的第一个参数都是需要操作的数组
+ */
+const Utils = {
+  concat: function () {
+    if (!(arguments[0] instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var results = []
+    for (var i = 0; i < arguments.length; i++) {
+      if (!(arguments[i] instanceof Array)) {
+        results[results.length] = arguments[i]
+        break;
+      }
+      for (var j = 0; j < arguments[i].length; j++) {
+        results[results.length] = arguments[i][j]
+      }
+    }
+    return results
+  },
+  every: function (arr, fn) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var bool = true
+    for (var i = 0; i < arr.length; i++) {
+      bool = fn(arr[i])
+      if (!bool) break
+    }
+    return bool
+  },
+  fill: (arr, value, start, end) => {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    start = start || 0
+    start = start < 0 ? start + arr.length : start
+    end = end ? (end < 0 ? arr.length + end : end) : arr.length
+    for (var i = start; i < end; i++) {
+      arr[i] = value
+    }
+    return arr
+  },
+  filter: function (arr, fn) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var results = []
+    for (var i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i))
+        results[results.length] = arr[i]
+    }
+    return results
+  },
+  find: function (arr, fn) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var el = undefined
+    for (var i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i)) {
+        el = arr[i]
+        break
+      }
+    }
+    return el
+  },
+  findIndex: function (arr, fn) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var index = -1
+    for (var i = 0; i < arr.length; i++) {
+      if (fn(arr[i], i)) {
+        index = i
+        break
+      }
+    }
+    return index
+  },
+  flat: function (arr, depth) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var results = []
+    for (var i = 0; i < arr.length; i++) {
+      if (!(arr[i] instanceof Array)) {
+        results[results.length] = arr[i]
+      } else {
+        for (var j = 0; j < arr[i].length; j++) {
+          if (depth && (arr[i][j] instanceof Array)) {
+            var dees = Utils.flat(arr[i][j], depth)
+            if (dees instanceof Array) {
+              for (var n = 0; n < dees.length; n++) {
+                results[results.length] = dees[n]
+              }
+            }
+          } else {
+            results[results.length] = arr[i][j]
+          }
+        }
+      }
+    }
+    return results
+  },
+  forEach: function (arr, fn) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    for (var i = 0; i < arr.length; i++) {
+      fn(arr[i], i)
+    }
+    return arr
+  },
+  includes: function (arr, value, fromIndex) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    fromIndex = fromIndex < 0 ? arr.length + fromIndex : fromIndex
+    for (var i = fromIndex; i < arr.length; i++) {
+      // 注意检测NaN
+      if ((arr[i] === value) || (value !== value && arr[i] !== arr[i])) return true
+    }
+    return false
+  },
+  indexOf: function (arr, value, fromIndex) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var num = -1
+    fromIndex = (fromIndex < 0 ? (fromIndex + arr.length < 0 ? 0 : fromIndex + arr.length) : fromIndex) || 0
+    for (var i = fromIndex; i < arr.length; i++) {
+      // 检测NaN
+      if ((arr[i] === value) || (value !== value && arr[i] !== arr[i])) {
+        num = i
+        break
+      }
+    }
+    return num
+  },
+  join: function (arr, seperator) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var str = "", len = arr.length - 1
+    seperator = seperator || ','
+    for (var i = 0; i < len; i++) {
+      str += arr[i] + seperator
+    }
+    str += arr[len]
+    return str
+  },
+  lastIndexOf: function (arr, value, fromIndex) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var num = -1
+    fromIndex = (fromIndex < 0 ? (fromIndex + arr.length < 0 ? 0 : fromIndex + arr.length) : fromIndex) || 0
+    for (var i = arr.length - 1; i > fromIndex; i--) {
+      // 检测NaN
+      if ((arr[i] === value) || (value !== value && arr[i] !== arr[i])) {
+        num = i
+        break
+      }
+    }
+    return num
+  },
+  map: function (arr, fn) {
+    if (!(arr instanceof Array)) throw new Error("请确保参数类型为数组")
+    var results = []
+    for (var i = 0; i < arr.length; i++) {
+      results[i] = fn(arr[i], i)
+    }
+    return results
+  },
+  pop: function (arr) {
+    if (!(arr instanceof Array)) throw new Error("请确保参数类型为数组")
+    var el = arr[arr.length - 1]
+    arr.length = arr.length - 1
+    return el
+  },
+  push: function (arr) {
+    if (!(arr instanceof Array)) throw new Error("请确保参数类型为数组")
+    for (var i = 1; i < arguments.length; i++) {
+      arr[arr.length] = arguments[i]
+    }
+    return arr.length
+  },
+  reduce: function (arr, fn, initialValue) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var accumulator = initialValue !== undefined ? initialValue : arr[0]
+    var j = initialValue !== undefined ? 0 : 1
+    for (var i = j; i < arr.length; i++) {
+      accumulator = fn(arr[i], accumulator, i + j)
+    }
+    return accumulator
+  },
+  reduceRight: function (arr, fn, initialValue) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var len = arr.length, accumulator = initialValue
+    if (initialValue === undefined) {
+      len = arr.length - 1
+      accumulator = arr[len]
+    }
+    while (len) {
+      accumulator = fn(arr[len - 1], accumulator, len - 1)
+      len--
+    }
+    return accumulator
+  },
+  reverse: function (arr) {
+    if (!(arr instanceof Array)) throw new Error("请确保参数类型为数组")
+    var i = Math.ceil(arr.length / 2)
+    for (var j = 0; j < i; j++) {
+      var temp = arr[j]
+      arr[j] = arr[arr.length - j - 1]
+      arr[arr.length - j - 1] = temp
+    }
+    return arr
+  },
+  shift: function (arr) {
+    if (!(arr instanceof Array)) throw new Error("请确保参数类型为数组")
+    var el = arr[0]
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = arr[i + 1]
+    }
+    arr.length = arr.length - 1
+    return el
+  },
+  slice: function (arr, begin, end) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    var results = []
+    begin = begin || 0
+    end = end || arr.length
+    if (begin < 0) begin += arr.length      // 如果begin < 0， begin = 数组长度+begin 
+    if (begin < 0) begin = 0                // 如果还小于0， 则直接修正为0
+    if (end < 0) end += arr.length          // 如果end < 0， end = 数组长度+end 
+    if (end > arr.length) end = arr.length  // 如果end > 数组长度， end = 数组长度
+    for (var i = begin; i < end; i++) {
+      results[results.length] = arr[i]
+    }
+    return results
+  },
+  splice: function (arr, start, deleteCount) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    start = start || 0
+    var rest = []
+    for (var j = 3; j < arguments.length; j++) {
+      rest[rest.length] = arguments[j]
+    }
+    var results = []
+    // 修正起始位置   小于0的话，就加上数组长度，如果还小于0，修正为0   大于数组长度的话 就修正为数组长度
+    start = start < 0 ?
+      (start + arr.length < 0 ? 0 : start + arr.length) :
+      (start > arr.length ? arr.length : start)
+    // 如果deleteCount数值过大，则修正为从起始位置到数组结尾的长度
+    if (deleteCount > (arr.length - start)) {deleteCount = arr.length - start}
+    // 获取删除的元素，并组成新的数组实例
+    for (var i = start; i < (start + deleteCount); i++) {
+      results[results.length] = arr[i]
+    }
+    // 元素删除之后，将所有数组元素响应的前置
+    for (var i = start + deleteCount; i < arr.length; i++) {
+      arr[i - deleteCount] = arr[i]
+    }
+    // 修正数组长度
+    arr.length = arr.length - deleteCount
+    // 将添加的元素填充到对应位置
+    for (var i = rest.length - 1; i >= 0; i--) {
+      for (var j = arr.length; j > start; j--) {
+        arr[j] = arr[j - 1]
+      }
+      arr[start] = rest[i]
+    }
+    return results
+  },
+  unshift: function (arr) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    for (var i = arguments.length - 1; i >= 1; i--) {
+      for (var j = arr.length; j > 0; j--) {
+        arr[j] = arr[j - 1]
+      }
+      arr[0] = arguments[i]
+    }
+    return arr.length
+  },
+  zip: function (arr, others, fn) {
+    if (!(arr instanceof Array)) throw new Error("请确保第一个参数类型为数组")
+    if (!(others instanceof Array)) throw new Error("请确保第二个参数类型为数组")
+    var results = []
+    var min = Math.min(arr.length, others.length)
+    for (var i = 0; i < min; i++) {
+      results[results.length] = fn(arr[i], others[i])
+    }
+    return results
+  }
+}
