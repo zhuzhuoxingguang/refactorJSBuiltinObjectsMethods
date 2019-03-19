f13ebee0e7287239af6fa4dcd36280f08f91d0a7
cauc <cauc@caucdeMacBook-Air.local>
2019-03-19 10:52:31 +0800

    add comment for splice function

--- src/array/ts/splice.ts
+++ src/array/ts/splice.ts
@@ -1,5 +1,11 @@
 // splice() 方法通过删除或替换现有元素来修改数组,并以数组形式返回被修改的内容。此方法会改变原数组。
-
+/**
+ * @return 返回删除元素所组成的数组实例
+ * @param arr 需要操作的数组
+ * @param start 开始位置
+ * @param deleteCount 需要删除的元素的个数
+ * @param rest 添加的元素，可多个
+ */
 const splice = (arr, start: number = 0, deleteCount: number = arr.length - start, ...rest) => {
   let results = []
   // 修正起始位置   小于0的话，就加上数组长度，如果还小于0，修正为0   大于数组长度的话 就修正为数组长度
